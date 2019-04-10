1f040335f8828908f293f6f949f0465d95616b02
Johan Chenaux <johan.chenaux@students.hevs.ch>
2019-04-10 11:42:58 +0200

    counter to be done

--- AhbLiteComponents/hdl/ahbUart_studentVersion.vhd
+++ AhbLiteComponents/hdl/ahbUart_studentVersion.vhd
@@ -50,6 +50,30 @@ ARCHITECTURE studentVersion OF ahbUart IS
   signal CtrlBuffer: std_ulogic_vector(CtrlBitNb-1 downto 0);
   signal BaudBuffer: std_ulogic_vector(prescalerBitNb-1 downto 0);
   signal readyToSend : std_ulogic; --set once char is stored in TxBuffer
+  
+  SIGNAL current_state_tx : STATE_TYPE_TX;
+  SIGNAL next_state_tx : STATE_TYPE_TX;
+   
+   TYPE STATE_TYPE_TX IS (
+    waitForChar,
+    startCounter,
+    sendStartBit,
+    sendBitX,
+    nextBit,
+    sendStopBit
+   );
+   
+  SIGNAL current_state_rx : STATE_TYPE_RX;
+  SIGNAL next_state_rx : STATE_TYPE_RX;
+  
+  TYPE STATE_TYPE_RX IS (
+    waitForChar,
+    startCounter,
+    readStartBit,
+    readBitX,
+    nextBit,
+    readStopBit
+   );
 
 BEGIN
   --------------------------------------------------------------------------------
@@ -115,13 +139,145 @@ BEGIN
       hRData <= std_ulogic_vector(RESIZE(unsigned(RxBuffer), ahbDataBitNb));
     end if;
   END PROCESS outRegProcess;
+  
+
+      
+  --------------------------------------------------------------------------------
+  -- transition for state machines
+  --------------------------------------------------------------------------------
+  state_machine_transition: PROCESS(hClk, hReset_n)
+  BEGIN
+    if hReset_n = '1' then
+      current_state_tx <= waitForChar; -- initial state
+      current_state_rx <= waitForChar; -- initial state
+    elsif rising_edge(hClk) then
+      current_state_rx <= next_state_rx; -- transition of states
+      current_state_tx <= next_state_tx; -- transition of states
+    end if;
+  END PROCESS state_machine_transition;
+  
+  --------------------------------------------------------------------------------
+  -- state machine for the Tx
+  --------------------------------------------------------------------------------
+   nextstate_tx_proc : PROCESS ( 
+      readyToSend,
+      counterRateTx,
+      counterPosBitTx,
+      current_state_tx
+   ) 
+  BEGIN
+    next_state_tx <= current_state_tx; -- default value
+    startCounterTx <= '0';
+    
+    CASE current_state_tx IS
+      -- currently waiting for a char to be written in the reg
+      WHEN waitForChar =>
+        CtrlBuffer(readyWriteBitPos) <= '1'; --uC can write a new char
+        IF readyToSend = '1' THEN
+          next_state_tx <= startCounter;
+        END IF;
+        
+      WHEN startCounter => 
+        -- reset the control bit : uC can't write a new char
+        CtrlBuffer(readyWriteBitPos) <= '0';
+        -- start counter for the impuls
+        startCounterTx <= '1';
+        next_state_tx <= sendStartBit;
+          
+      WHEN sendStartBit => 
+        Txd <= '0'; -- startBit
+        if counterRateTx = '1' then
+          next_state_tx <= sendBitX;
+        end if;
+             
+      WHEN sendBitX =>
+         TxD <= TxBuffer(counterPosBitTx); --bit x of the buffer tx
+        if counterRateTx = '1' then
+          next_state_tx <= nextBit;
+        end if;
+          
+      WHEN nextBit => 
+        if counterPosBitTx = 7 then
+          next_state_tx <= sendStopBit;
+        else
+          next_state_tx <= sendBitX;
+        end if;
+          
+      WHEN sendStopBit =>
+        TxD <= '1'; --stop bit
+        if counterRateTx = '1' then
+          next_state_tx <= waitForChar;
+        end if;
+     END CASE; 
+  END PROCESS nextstate_tx_proc;  
+
+
+  --------------------------------------------------------------------------------
+  -- state machine for the rx
+  --------------------------------------------------------------------------------
+   nextstate_rx_proc : PROCESS ( 
+      counterRateRx,
+      counterPosBitRx,
+      current_state_rx
+   ) 
+  BEGIN
+    next_state_rx <= current_state_rx; -- default value
+    startCounterRx <= '0';
+    CtrlBuffer(readyReadBitPos) <= '0';
+    CtrlBuffer(currentlyReceivingBitPos) <= '1';
+    
+--        waitForChar,
+--    startCounter,
+--    readStartBit,
+--    readBitX,
+--    nextBit,
+--    readStopBit
+    
+    
+    CASE current_state_rx IS
+      -- waiting for a start bit
+      WHEN waitForChar =>
+        CtrlBuffer(currentlyReceivingBitPos) <= '0'; -- not receiving
+        CtrlBuffer(readyReadBitPos) <= '1'; -- ready to read
+        IF RxD = '0' THEN
+          next_state_rx <= startCounter;
+        END IF;
+        
+      WHEN startCounter => 
+        CtrlBuffer(currentlyReceivingBitPos) <= '1'; --receiving
+        CtrlBuffer(readyReadBitPos) <= '0'; -- not ready to read
+        -- start counter for the impuls
+        startCounterRx <= '1';
+        next_state_rx <= readStartBit;
+        
+      WHEN readStartBit => 
+        if counterRateRx = '1' then
+          next_state_rx <= readBitX;
+        end if;
+             
+      WHEN readBitX =>
+        RxBuffer(counterPosBitRx) <= RxD; --bit x of the buffer rx
+        if counterRateRx = '1' then
+          next_state_rx <= nextBit;
+        end if;
+          
+      WHEN nextBit => 
+        if counterPosBitRx = 7 then
+          next_state_rx <= readStopBit;
+        else
+          next_state_rx <= readBitX;
+        end if;
+          
+      WHEN readStopBit =>
+        if counterRate = '1' then
+          next_state_rx <= waitForChar;
+        end if;
+     END CASE; 
+  END PROCESS nextstate_rx_proc;  
 
   -- AHB-Lite
-  hRData  <=	(OTHERS => '0');
-  hReady  <=	'0';	
+  hReady  <=	'1';	
   hResp	  <=	'0';	
-  
-  -- Serial
-  TxD <= '0';
+
 END ARCHITECTURE studentVersion;
 
