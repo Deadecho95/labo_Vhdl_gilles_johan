1d997b53702e1c37918c56f12b7c99b3a7fd4fdf
Johan Chenaux <johan.chenaux@students.hevs.ch>
2019-04-16 11:42:51 +0200

    uart works

--- AhbLiteComponents/hdl/ahbUart_studentVersion.vhd
+++ AhbLiteComponents/hdl/ahbUart_studentVersion.vhd
@@ -26,15 +26,15 @@ ARCHITECTURE studentVersion OF ahbUart IS
   -- constants for the bit number
   constant addrRegBitNb : positive := 2; 
   constant CtrlBitNb : positive := 3;
-  constant prescalerBitNb : posititve := 8;
+  constant prescalerBitNb : positive := 8;
   
   -- constants for the register addresses
-  constant baudBufferAddr : positive := 2;
-  constant CtrlBufferAddr : positive := 1;
-  constant TxBufferAddr : positive := 0;
+  constant baudBufferAddr : std_ulogic_vector(addrRegBitNb-1 downto 0) := "10";
+  constant CtrlBufferAddr : std_ulogic_vector(addrRegBitNb-1 downto 0) := "01";
+  constant TxBufferAddr : std_ulogic_vector(addrRegBitNb-1 downto 0) := "00";
   
   -- constants for the position of bits in CtrlBuffer
-  constant readyReadBitPos : positive := 0;
+  constant readyReadBitPos : integer := 0;
   constant readyWriteBitPos : positive := 1;
   constant currentlyReceivingBitPos : positive := 2;
    
@@ -51,29 +51,43 @@ ARCHITECTURE studentVersion OF ahbUart IS
   signal BaudBuffer: std_ulogic_vector(prescalerBitNb-1 downto 0);
   signal readyToSend : std_ulogic; --set once char is stored in TxBuffer
   
-  SIGNAL current_state_tx : STATE_TYPE_TX;
-  SIGNAL next_state_tx : STATE_TYPE_TX;
-   
-   TYPE STATE_TYPE_TX IS (
+  -- signals for counters
+  signal counterPosBitTx : unsigned(2 downto 0);
+  signal counterPosBitRx : unsigned(2 downto 0);
+  signal incrPosBitTx : std_ulogic;
+  signal incrPosBitRx : std_ulogic;
+  signal counterRateRx : std_ulogic;
+  signal counterRateTx : std_ulogic;
+  signal countTimeRx : unsigned(BaudBuffer'range);
+  signal countTimeTx : unsigned(BaudBuffer'range);
+  signal startCounterTx : std_ulogic;
+  signal startCounterRx : std_ulogic;  
+  
+  signal clearCtrlReadyRead: std_ulogic;
+  signal setCtrlReadyRead: std_ulogic;
+  
+
+  TYPE STATE_TYPE_TX IS (
     waitForChar,
     startCounter,
     sendStartBit,
     sendBitX,
-    nextBit,
     sendStopBit
    );
-   
-  SIGNAL current_state_rx : STATE_TYPE_RX;
-  SIGNAL next_state_rx : STATE_TYPE_RX;
-  
+
   TYPE STATE_TYPE_RX IS (
     waitForChar,
     startCounter,
     readStartBit,
     readBitX,
-    nextBit,
     readStopBit
    );
+        
+  SIGNAL current_state_tx : STATE_TYPE_TX;
+  SIGNAL next_state_tx : STATE_TYPE_TX;
+   
+  SIGNAL current_state_rx : STATE_TYPE_RX;
+  SIGNAL next_state_rx : STATE_TYPE_RX;
 
 BEGIN
   --------------------------------------------------------------------------------
@@ -83,7 +97,7 @@ BEGIN
   BEGIN
     IF hReset_n = '0' THEN 
       hTransInt <= (OTHERS => '0');
-      addrInt <= '0';
+      addrInt <= (OTHERS => '0');
       hSelInt <= '0';
       hWriteInt <= '0';
       
@@ -92,7 +106,7 @@ BEGIN
       hSelInt <= hSel;
       hWriteInt <= hWrite;
       if hSel = '1'  then --save addr to be in phase with data
-        addrInt <= hAddr(1 downto 0);
+        addrInt <= std_ulogic_vector(resize(unsigned(hAddr),addrInt'length));
       end if;
     END IF;
   END PROCESS shiftProcess;
@@ -104,20 +118,18 @@ BEGIN
   BEGIN 
     IF hReset_n = '0' THEN 
       TxBuffer <= (OTHERS => '0');
-      CtrlBuffer <= (OTHERS => '0');
       BaudBuffer <= (OTHERS => '0');
       
     ELSIF rising_edge(hClk) THEN
       readyToSend <= '0'; --default value, set only when TxBuffer is written
       if hSelInt = '1' and hTransInt = transNonSeq then
-        IF addrInt = TxBufferAddr THEN -- work with tx buffer
-          if hWriteInt = '1'  and ctrlBuffer(readyWriteBitPos) = '0' then
+        IF addrInt = TxBufferAddr and hWriteInt = '1' THEN -- work with tx buffer
+          if ctrlBuffer(readyWriteBitPos) = '0' then --hWriteInt = '1' and
             TxBuffer <= std_ulogic_vector(RESIZE(unsigned(hWData), txFifoDepth));
             readyToSend <= '1';
           end if;
         ELSIF addrInt = CtrlBufferAddr THEN-- work with control buffer
           if hWriteInt = '1' then
-            CtrlBuffer <= std_ulogic_vector(RESIZE(unsigned(hWData), 3));
           end if;
         ELSIF addrInt = baudBufferAddr THEN-- work with baud register
           if hWriteInt = '1' then
@@ -131,23 +143,40 @@ BEGIN
   --------------------------------------------------------------------------------
   -- read registers from uC
   --------------------------------------------------------------------------------
-  outRegProcess: PROCESS(hAddrInt)
+  outRegProcess: PROCESS(addrInt, CtrlBuffer, RxBuffer)
   BEGIN
+    clearCtrlReadyRead <= '0';
     if addrInt = CtrlBufferAddr then
       hRData <= std_ulogic_vector(RESIZE(unsigned(CtrlBuffer), ahbDataBitNb));
     else
       hRData <= std_ulogic_vector(RESIZE(unsigned(RxBuffer), ahbDataBitNb));
+      clearCtrlReadyRead <= '1';
     end if;
   END PROCESS outRegProcess;
   
-
+  --------------------------------------------------------------------------------
+  -- set resetfor control bits
+  --------------------------------------------------------------------------------
+  setResetCtrlBits: PROCESS(hReset_n, hClk)
+  BEGIN
+    
+    if hReset_n = '0' then
+      CtrlBuffer(readyReadBitPos) <= '0';
+    elsif rising_edge(hClk) then
+      if setCtrlReadyRead ='1' then
+        CtrlBuffer(readyReadBitPos) <= '1';
+      elsif clearCtrlReadyRead = '1' then
+        CtrlBuffer(readyReadBitPos) <= '0';
+      end if;
+    end if;
+  END PROCESS setResetCtrlBits;
       
   --------------------------------------------------------------------------------
   -- transition for state machines
   --------------------------------------------------------------------------------
   state_machine_transition: PROCESS(hClk, hReset_n)
   BEGIN
-    if hReset_n = '1' then
+    if hReset_n = '0' then
       current_state_tx <= waitForChar; -- initial state
       current_state_rx <= waitForChar; -- initial state
     elsif rising_edge(hClk) then
@@ -167,43 +196,44 @@ BEGIN
    ) 
   BEGIN
     next_state_tx <= current_state_tx; -- default value
-    startCounterTx <= '0';
+    incrPosBitTx <= '0';
+    Txd <= '1';
     
     CASE current_state_tx IS
       -- currently waiting for a char to be written in the reg
       WHEN waitForChar =>
-        CtrlBuffer(readyWriteBitPos) <= '1'; --uC can write a new char
+        startCounterTx <= '0';
+        CtrlBuffer(readyWriteBitPos) <= '0'; --uC can write a new char
         IF readyToSend = '1' THEN
           next_state_tx <= startCounter;
         END IF;
         
       WHEN startCounter => 
         -- reset the control bit : uC can't write a new char
-        CtrlBuffer(readyWriteBitPos) <= '0';
+        CtrlBuffer(readyWriteBitPos) <= '1';
         -- start counter for the impuls
         startCounterTx <= '1';
         next_state_tx <= sendStartBit;
           
       WHEN sendStartBit => 
         Txd <= '0'; -- startBit
+        startCounterTx <= '1';
         if counterRateTx = '1' then
           next_state_tx <= sendBitX;
         end if;
-             
+
       WHEN sendBitX =>
-         TxD <= TxBuffer(counterPosBitTx); --bit x of the buffer tx
+        TxD <= TxBuffer(to_integer(UNSIGNED(counterPosBitTx)));
+        startCounterTx <= '1';
         if counterRateTx = '1' then
-          next_state_tx <= nextBit;
-        end if;
-          
-      WHEN nextBit => 
-        if counterPosBitTx = 7 then
-          next_state_tx <= sendStopBit;
-        else
-          next_state_tx <= sendBitX;
+          incrPosBitTx <= '1';
+          if to_integer(UNSIGNED(counterPosBitTx)) = 7 then
+            next_state_tx <= sendStopBit;
+          end if;
         end if;
           
       WHEN sendStopBit =>
+        startCounterTx <= '1';
         TxD <= '1'; --stop bit
         if counterRateTx = '1' then
           next_state_tx <= waitForChar;
@@ -218,66 +248,135 @@ BEGIN
    nextstate_rx_proc : PROCESS ( 
       counterRateRx,
       counterPosBitRx,
-      current_state_rx
+      current_state_rx,
+      RxD
    ) 
   BEGIN
     next_state_rx <= current_state_rx; -- default value
-    startCounterRx <= '0';
-    CtrlBuffer(readyReadBitPos) <= '0';
     CtrlBuffer(currentlyReceivingBitPos) <= '1';
-    
---        waitForChar,
---    startCounter,
---    readStartBit,
---    readBitX,
---    nextBit,
---    readStopBit
-    
+    incrPosBitRx <= '0';
+    setCtrlReadyRead <= '0';
     
     CASE current_state_rx IS
       -- waiting for a start bit
       WHEN waitForChar =>
+        startCounterRx <= '0';
         CtrlBuffer(currentlyReceivingBitPos) <= '0'; -- not receiving
-        CtrlBuffer(readyReadBitPos) <= '1'; -- ready to read
         IF RxD = '0' THEN
           next_state_rx <= startCounter;
         END IF;
         
       WHEN startCounter => 
         CtrlBuffer(currentlyReceivingBitPos) <= '1'; --receiving
-        CtrlBuffer(readyReadBitPos) <= '0'; -- not ready to read
         -- start counter for the impuls
         startCounterRx <= '1';
         next_state_rx <= readStartBit;
         
       WHEN readStartBit => 
+        startCounterRx <= '1';
         if counterRateRx = '1' then
           next_state_rx <= readBitX;
         end if;
-             
-      WHEN readBitX =>
-        RxBuffer(counterPosBitRx) <= RxD; --bit x of the buffer rx
+
+       WHEN readBitX =>
+        RxBuffer(to_integer(UNSIGNED(counterPosBitRx))) <= RxD; --bit x of the buffer rx
+        startCounterRx <= '1';
         if counterRateRx = '1' then
-          next_state_rx <= nextBit;
+          incrPosBitRx <= '1';
+          if to_integer(UNSIGNED(counterPosBitRx)) = 7 then
+            next_state_rx <= readStopBit;
+          end if;
         end if;
-          
-      WHEN nextBit => 
-        if counterPosBitRx = 7 then
-          next_state_rx <= readStopBit;
-        else
-          next_state_rx <= readBitX;
-        end if;
+      
           
       WHEN readStopBit =>
-        if counterRate = '1' then
+        setCtrlReadyRead <= '1';
+        startCounterRx <= '1';
+        if counterRateRx = '1' then
           next_state_rx <= waitForChar;
         end if;
      END CASE; 
   END PROCESS nextstate_rx_proc;  
+
+  --------------------------------------------------------------------------------
+  -- timeCounterTx
+  --------------------------------------------------------------------------------
+  timeCounterTx: PROCESS(hClk, hReset_n)
+  BEGIN
+    IF hReset_n = '0' THEN
+      countTimeTx <= (others => '0');
+      
+    ELSIF rising_edge(hClk) THEN
+      counterRateTx <= '0';
+      if startCounterTx = '1' then
+      countTimeTx <= countTimeTx + 1;
+        IF countTimeTx+1 = unsigned(BaudBuffer) THEN
+          countTimeTx <= (others => '0');
+          counterRateTx <= '1'; --signal done
+        END IF;
+      else
+        countTimeTx <= (others => '0'); 
+      end if;
+    END IF;
+  END PROCESS timeCounterTx;
+  
+  --------------------------------------------------------------------------------
+  -- timeCounterRx
+  --------------------------------------------------------------------------------
+  timeCounterRx: PROCESS(hClk, hReset_n)
+  BEGIN
+    IF hReset_n = '0' THEN
+      countTimeRx <= (others => '0');
+      
+    ELSIF rising_edge(hClk) THEN
+      counterRateRx <= '0';
+      if startCounterRx = '1' then
+      countTimeRx <= countTimeRx + 1;
+        IF countTimeRx+1 = unsigned(BaudBuffer) THEN
+          countTimeRx <= (others => '0');
+        ELSIF countTimeRx = shift_right(unsigned(BaudBuffer), 1)-1 THEN
+          counterRateRx <= '1'; --signal done
+        END IF; 
+      else
+        countTimeRx <= (others => '0');
+      end if;
+    END IF;
+  END PROCESS timeCounterRx;
+    
+  --------------------------------------------------------------------------------
+  -- posBitCounterRx
+  --------------------------------------------------------------------------------
+  posBitCounterRx: PROCESS(hClk, hReset_n)
+  BEGIN
+    IF hReset_n = '0' THEN
+      counterPosBitRx <= (others => '0');
+      
+    ELSIF rising_edge(hClk) THEN
+      if incrPosBitRx = '1' then
+      counterPosBitRx <= counterPosBitRx + 1; 
+      end if;
+    END IF;
+  END PROCESS posBitCounterRx;
+  
+  --------------------------------------------------------------------------------
+  -- posBitCounterTx
+  --------------------------------------------------------------------------------
+  posBitCounterTx: PROCESS(hClk, hReset_n)
+  BEGIN
+    IF hReset_n = '0' THEN
+      counterPosBitTx <= (others => '0');
+      
+    ELSIF rising_edge(hClk) THEN
+      if incrPosBitTx = '1' then
+      counterPosBitTx <= counterPosBitTx + 1; 
+      end if;
+    END IF;
+  END PROCESS posBitCounterTx;
+  
+  --------------------------------------------------------------------------------
 
   -- AHB-Lite
   hReady  <=	'1';	
   hResp	  <=	'0';	
 
 END ARCHITECTURE studentVersion;
-
--- Simulation/ahbUart.do
+++ Simulation/ahbUart.do
@@ -1,35 +1,45 @@
 onerror {resume}
 quietly WaveActivateNextPane {} 0
-add wave -noupdate /ahbuart_tb/hreset_n
-add wave -noupdate /ahbuart_tb/hclk
+add wave -noupdate /ahbuart_tb/I_tester/testInformation
+add wave -noupdate /ahbuart_tb/hReset_n
+add wave -noupdate /ahbuart_tb/hClk
 add wave -noupdate -divider {AMBA bus}
-add wave -noupdate -radix hexadecimal /ahbuart_tb/haddr
-add wave -noupdate /ahbuart_tb/htrans
-add wave -noupdate /ahbuart_tb/hsel
-add wave -noupdate /ahbuart_tb/hwrite
-add wave -noupdate -radix hexadecimal /ahbuart_tb/hwdata
-add wave -noupdate -radix hexadecimal -subitemconfig {/ahbuart_tb/hrdata(15) {-height 15 -radix hexadecimal} /ahbuart_tb/hrdata(14) {-height 15 -radix hexadecimal} /ahbuart_tb/hrdata(13) {-height 15 -radix hexadecimal} /ahbuart_tb/hrdata(12) {-height 15 -radix hexadecimal} /ahbuart_tb/hrdata(11) {-height 15 -radix hexadecimal} /ahbuart_tb/hrdata(10) {-height 15 -radix hexadecimal} /ahbuart_tb/hrdata(9) {-height 15 -radix hexadecimal} /ahbuart_tb/hrdata(8) {-height 15 -radix hexadecimal} /ahbuart_tb/hrdata(7) {-height 15 -radix hexadecimal} /ahbuart_tb/hrdata(6) {-height 15 -radix hexadecimal} /ahbuart_tb/hrdata(5) {-height 15 -radix hexadecimal} /ahbuart_tb/hrdata(4) {-height 15 -radix hexadecimal} /ahbuart_tb/hrdata(3) {-height 15 -radix hexadecimal} /ahbuart_tb/hrdata(2) {-height 15 -radix hexadecimal} /ahbuart_tb/hrdata(1) {-height 15 -radix hexadecimal} /ahbuart_tb/hrdata(0) {-height 15 -radix hexadecimal}} /ahbuart_tb/hrdata
-add wave -noupdate /ahbuart_tb/hready
-add wave -noupdate /ahbuart_tb/hresp
+add wave -noupdate -radix hexadecimal /ahbuart_tb/hAddr
+add wave -noupdate /ahbuart_tb/hTrans
+add wave -noupdate /ahbuart_tb/hSel
+add wave -noupdate /ahbuart_tb/hWrite
+add wave -noupdate -radix hexadecimal /ahbuart_tb/hWData
+add wave -noupdate -radix hexadecimal -childformat {{/ahbuart_tb/hRData(15) -radix hexadecimal} {/ahbuart_tb/hRData(14) -radix hexadecimal} {/ahbuart_tb/hRData(13) -radix hexadecimal} {/ahbuart_tb/hRData(12) -radix hexadecimal} {/ahbuart_tb/hRData(11) -radix hexadecimal} {/ahbuart_tb/hRData(10) -radix hexadecimal} {/ahbuart_tb/hRData(9) -radix hexadecimal} {/ahbuart_tb/hRData(8) -radix hexadecimal} {/ahbuart_tb/hRData(7) -radix hexadecimal} {/ahbuart_tb/hRData(6) -radix hexadecimal} {/ahbuart_tb/hRData(5) -radix hexadecimal} {/ahbuart_tb/hRData(4) -radix hexadecimal} {/ahbuart_tb/hRData(3) -radix hexadecimal} {/ahbuart_tb/hRData(2) -radix hexadecimal} {/ahbuart_tb/hRData(1) -radix hexadecimal} {/ahbuart_tb/hRData(0) -radix hexadecimal}} -subitemconfig {/ahbuart_tb/hRData(15) {-height 15 -radix hexadecimal} /ahbuart_tb/hRData(14) {-height 15 -radix hexadecimal} /ahbuart_tb/hRData(13) {-height 15 -radix hexadecimal} /ahbuart_tb/hRData(12) {-height 15 -radix hexadecimal} /ahbuart_tb/hRData(11) {-height 15 -radix hexadecimal} /ahbuart_tb/hRData(10) {-height 15 -radix hexadecimal} /ahbuart_tb/hRData(9) {-height 15 -radix hexadecimal} /ahbuart_tb/hRData(8) {-height 15 -radix hexadecimal} /ahbuart_tb/hRData(7) {-height 15 -radix hexadecimal} /ahbuart_tb/hRData(6) {-height 15 -radix hexadecimal} /ahbuart_tb/hRData(5) {-height 15 -radix hexadecimal} /ahbuart_tb/hRData(4) {-height 15 -radix hexadecimal} /ahbuart_tb/hRData(3) {-height 15 -radix hexadecimal} /ahbuart_tb/hRData(2) {-height 15 -radix hexadecimal} /ahbuart_tb/hRData(1) {-height 15 -radix hexadecimal} /ahbuart_tb/hRData(0) {-height 15 -radix hexadecimal}} /ahbuart_tb/hRData
+add wave -noupdate /ahbuart_tb/hReady
+add wave -noupdate /ahbuart_tb/hResp
 add wave -noupdate -divider Registers
-add wave -noupdate -radix hexadecimal /ahbuart_tb/i_dut/dataoutregister
-add wave -noupdate -radix hexadecimal /ahbuart_tb/i_dut/controlregister
-add wave -noupdate -radix hexadecimal /ahbuart_tb/i_dut/scalerregister
-add wave -noupdate -radix hexadecimal /ahbuart_tb/i_dut/datainregister
-add wave -noupdate -radix hexadecimal -subitemconfig {/ahbuart_tb/i_dut/statusregister(15) {-radix hexadecimal} /ahbuart_tb/i_dut/statusregister(14) {-radix hexadecimal} /ahbuart_tb/i_dut/statusregister(13) {-radix hexadecimal} /ahbuart_tb/i_dut/statusregister(12) {-radix hexadecimal} /ahbuart_tb/i_dut/statusregister(11) {-radix hexadecimal} /ahbuart_tb/i_dut/statusregister(10) {-radix hexadecimal} /ahbuart_tb/i_dut/statusregister(9) {-radix hexadecimal} /ahbuart_tb/i_dut/statusregister(8) {-radix hexadecimal} /ahbuart_tb/i_dut/statusregister(7) {-radix hexadecimal} /ahbuart_tb/i_dut/statusregister(6) {-radix hexadecimal} /ahbuart_tb/i_dut/statusregister(5) {-radix hexadecimal} /ahbuart_tb/i_dut/statusregister(4) {-radix hexadecimal} /ahbuart_tb/i_dut/statusregister(3) {-radix hexadecimal} /ahbuart_tb/i_dut/statusregister(2) {-radix hexadecimal} /ahbuart_tb/i_dut/statusregister(1) {-radix hexadecimal} /ahbuart_tb/i_dut/statusregister(0) {-radix hexadecimal}} /ahbuart_tb/i_dut/statusregister
+add wave -noupdate /ahbuart_tb/I_DUT/BaudBuffer
+add wave -noupdate /ahbuart_tb/I_DUT/RxBuffer
+add wave -noupdate /ahbuart_tb/I_DUT/TxBuffer
+add wave -noupdate /ahbuart_tb/I_DUT/CtrlBuffer
 add wave -noupdate -divider Internals
-add wave -noupdate /ahbuart_tb/i_dut/txen
-add wave -noupdate -radix unsigned /ahbuart_tb/i_dut/txshiftcounter
-add wave -noupdate /ahbuart_tb/i_dut/txsending
-add wave -noupdate /ahbuart_tb/i_dut/rxen
-add wave -noupdate -radix unsigned /ahbuart_tb/i_dut/rxshiftcounter
-add wave -noupdate /ahbuart_tb/i_dut/rxreceiving
-add wave -noupdate /ahbuart_tb/i_dut/rxdataready
+add wave -noupdate /ahbuart_tb/I_DUT/addrInt
+add wave -noupdate /ahbuart_tb/I_DUT/clearCtrlReadyRead
+add wave -noupdate /ahbuart_tb/I_DUT/hSelInt
+add wave -noupdate /ahbuart_tb/I_DUT/hTransInt
+add wave -noupdate /ahbuart_tb/I_DUT/hWriteInt
+add wave -noupdate /ahbuart_tb/I_DUT/current_state_tx
+add wave -noupdate /ahbuart_tb/I_DUT/next_state_tx
+add wave -noupdate /ahbuart_tb/I_DUT/current_state_rx
+add wave -noupdate /ahbuart_tb/I_DUT/next_state_rx
+add wave -noupdate -divider Counters
+add wave -noupdate -format Analog-Step -height 74 -max 16.0 /ahbuart_tb/I_DUT/counterPosBitRx
+add wave -noupdate -format Analog-Step -height 74 -max 16.0 /ahbuart_tb/I_DUT/counterPosBitTx
+add wave -noupdate /ahbuart_tb/I_DUT/counterRateRx
+add wave -noupdate /ahbuart_tb/I_DUT/counterRateTx
+add wave -noupdate -format Analog-Step -height 74 -max 5.0 /ahbuart_tb/I_DUT/countTimeRx
+add wave -noupdate -format Analog-Step -height 74 -max 5.0 /ahbuart_tb/I_DUT/countTimeTx
 add wave -noupdate -divider UART
-add wave -noupdate /ahbuart_tb/txd
-add wave -noupdate /ahbuart_tb/rxd
+add wave -noupdate /ahbuart_tb/TxD
+add wave -noupdate /ahbuart_tb/RxD
 TreeUpdate [SetDefaultTree]
-WaveRestoreCursors {{Cursor 1} {0 ps} 0}
+WaveRestoreCursors {{Cursor 1} {1015260 ps} 0}
+quietly wave cursor active 1
 configure wave -namecolwidth 255
 configure wave -valuecolwidth 55
 configure wave -justifyvalue left
@@ -44,4 +54,4 @@ configure wave -griddelta 40
 configure wave -timeline 0
 configure wave -timelineunits us
 update
-WaveRestoreZoom {0 ps} {4200 ns}
+WaveRestoreZoom {0 ps} {4263 ns}
